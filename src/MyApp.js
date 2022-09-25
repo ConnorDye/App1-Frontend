473d3be51ef4f8d6fba7021156b675f7d672886b
connor <connor.h.dye@gmail.com>
2022-09-25 11:18:06 -0700

    =App Working Copy

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import Table from './Table';
+import Form from './Form';
 
 // REPLACE THIS WITH HOOK SO WE CAN ALTER STATE IN OTHER COMPONENTS
 // const characters = [
@@ -24,24 +25,25 @@ import Table from './Table';
 
 // Our data is officially contained in the state owned by the MyApp component (rather contained as a constant in the render function).
 function MyApp() {
-  const [characters, setCharacters] = useState([
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ]);  
+  // const [characters, setCharacters] = useState([
+  //     {
+  //       name: 'Charlie',
+  //       job: 'Janitor',
+  //     },
+  //     {
+  //       name: 'Mac',
+  //       job: 'Bouncer',
+  //     },
+  //     {
+  //       name: 'Dee',
+  //       job: 'Aspring actress',
+  //     },
+  //     {
+  //       name: 'Dennis',
+  //       job: 'Bartender',
+  //     },
+  //   ]);  
+  const [characters, setCharacters] = useState([]);
 
 function removeOneCharacter (index) {
   const updated = characters.filter((character, i) => {
@@ -49,10 +51,15 @@ function removeOneCharacter (index) {
     });
     setCharacters(updated);
   }
+  
+function updateList(person) {
+    setCharacters([...characters, person]);
+  }
 
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
     </div>
   )
 
